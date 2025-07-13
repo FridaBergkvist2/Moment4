a9ff85ee07153ddadec40f66bb3de15993ab64b0
FridaBergkvist2 <fridaber10@student.astar.se>
2025-07-13 22:33:50 +0200

    Lagt till Test fil och System

---
+++ System.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+class System
+{
+    static void Main()
+    {
+        string path = "Fridas_test_fil.txt";
+
+        if (File.Exists(path))
+        {
+            string innehall = File.ReadAllText(path);
+            Console.WriteLine("Filens innehåll:");
+            Console.WriteLine(innehall);
+        }
+        else
+        {
+            Console.WriteLine("Filen finns inte.");
+        }
+    }
+}
