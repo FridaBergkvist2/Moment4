122e530885742a843fa87d148b592b23de5c7994
FridaBergkvist2 <fridaber10@student.astar.se>
2025-07-13 22:52:33 +0200

    Lagt till Program och Produkter

---
+++ Produkter.cs
@@ -0,0 +1,10 @@
+﻿public class ProduktLåda<T>
+{
+    public T Innehåll { get; set; }
+
+    public void VisaInfo()
+    {
+        Console.WriteLine("Lådan innehåller:");
+        Console.WriteLine(Innehåll.ToString());
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿bool kör = true;
+
+while (kör)
+{
+    try
+    {
+        Console.Write("Ange produktnamn (eller 'stopp'): ");
+        string namn = Console.ReadLine();
+        if (namn.ToLower() == "stopp")
+        {
+            kör = false;
+            continue;
+        }
+
+        Console.Write("Ange pris: ");
+        double pris = Convert.ToDouble(Console.ReadLine());
+
+produkter.Add(new Produkt(namn, pris));
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Fel: Du måste skriva ett giltigt pris (t.ex. 199,50).");
+    }
+}
