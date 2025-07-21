03688820d9039caa6a3d4f9bfa47c540629fbc61
FridaBergkvist2 <fridaber10@student.astar.se>
2025-07-21 18:42:38 +0200

    Nu har jag uppdaterat filerna i rätt mment

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Frida heter jag");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Storage<Produkt> produktStorage = new Storage<Produkt>();
+List<Produkt> produkter = new List<Produkt>();
+        bool kör = true;
+
+while (kör)
+{
+    try
+    {
+        Console.Write("Ange produktnamn (eller 'stopp'): ");
+        string namn = Console.ReadLine();
+        if (namn.ToLower() == "stopp")
+        {
+            kör = false;
+            continue;
+        }
+
+        Console.Write("Ange pris: ");
+        double pris = Convert.ToDouble(Console.ReadLine());
+
+        produkter.Add(new Produkt(namn, pris));
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Fel: Du måste skriva ett giltigt pris (t.ex. 199,50).");
+    }
+}
+        foreach (var produkt in produkter)
+        {
+            produktStorage.LäggTill(produkt);
+        }
+ 
+Console.WriteLine("\nProdukter i Storage:"); 
+produktStorage.SkrivUtAlla();
+
+    }
+}
---
+++ Storage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Storage<T>
+{
+    private List<T> lista = new List<T>();
+    
+public void LäggTill(T objekt)
+    {
+        lista.Add(objekt);
+    }
+
+    public void SkrivUtAlla()
+    {
+        Console.WriteLine("Innehåll i Storage:");
+        foreach (T objekt in lista)
+        {
+            Console.WriteLine(objekt);
+        }
+    }
+}
